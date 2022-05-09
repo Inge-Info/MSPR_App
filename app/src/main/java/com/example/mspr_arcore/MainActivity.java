406d8c2f75661f61cd9926cfb03e596de4b2b5e4
Etienne <Etienne@DESKTOP-6NHT89T>
2022-05-09 20:05:01 +0200

    Ajout de log et initialisation d'une session ar core dans la fonction onCreate et ajout de la fonction configCam pour configurer la caméra

--- app/src/main/java/com/example/mspr_arcore/MainActivity.java
+++ app/src/main/java/com/example/mspr_arcore/MainActivity.java
@@ -19,6 +19,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.CameraConfig;
+import com.google.ar.core.CameraConfigFilter;
 import com.google.ar.core.Config;
 import com.google.ar.core.Session;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
@@ -27,6 +29,9 @@ import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 import com.google.ar.core.exceptions.UnavailableException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+
+import java.util.EnumSet;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +47,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mArButton = findViewById(R.id.mArButton);
         //AffichageImg = findViewById(R.id.AffichageImgId );
+
+        // Check if ar core is supported and up to date then create a session
+        if (isARCoreSupportedAndUpToDate()) {
+            createSession();
+            Log.i("CreateSession ", "reussis");
+        } else {
+            Log.i("CreateSession ", "non réussis");
+        }
+
+        configCam();
+
     }
 
     @Override
@@ -58,6 +74,11 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    // requestInstall(Activity, true) will triggers installation of
+    // Google Play Services for AR if necessary.
+    private boolean mUserRequestedInstall = true;
+
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -68,9 +89,39 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // Check if ar core is supported and up to date then create a session
-        if (isARCoreSupportedAndUpToDate()) {
-            createSession();
+        // Ensure that Google Play Services for AR and ARCore device profile data are
+        // installed and up to date.
+        try {
+            if (session == null) {
+                switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    case INSTALLED:
+                        // Success: Safe to create the AR session.
+                        session = new Session(this);
+                        break;
+                    case INSTALL_REQUESTED:
+                        // When this method returns `INSTALL_REQUESTED`:
+                        // 1. ARCore pauses this activity.
+                        // 2. ARCore prompts the user to install or update Google Play
+                        //    Services for AR (market://details?id=com.google.ar.core).
+                        // 3. ARCore downloads the latest device profile data.
+                        // 4. ARCore resumes this activity. The next invocation of
+                        //    requestInstall() will either return `INSTALLED` or throw an
+                        //    exception if the installation or update did not succeed.
+                        mUserRequestedInstall = false;
+                }
+            }
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                    .show();
+    } catch (UnavailableDeviceNotCompatibleException e) {
+            e.printStackTrace();
+        } catch (UnavailableSdkTooOldException e) {
+            e.printStackTrace();
+        } catch (UnavailableArcoreNotInstalledException e) {
+            e.printStackTrace();
+        } catch (UnavailableApkTooOldException e) {
+            e.printStackTrace();
         }
     }
 
@@ -149,5 +200,27 @@ public class MainActivity extends AppCompatActivity {
                 // Handle the error appropriately.
         }
         return false;
+    }
+
+    private void configCam() {
+        // Create a camera config filter for the session.
+        CameraConfigFilter filter = new CameraConfigFilter(session);
+
+        // Return only camera configs that target 30 fps camera capture frame rate.
+        filter.setTargetFps(EnumSet.of(CameraConfig.TargetFps.TARGET_FPS_30));
+
+        // Return only camera configs that will not use the depth sensor.
+        filter.setDepthSensorUsage(EnumSet.of(CameraConfig.DepthSensorUsage.DO_NOT_USE));
+
+        // Get list of configs that match filter settings.
+        // In this case, this list is guaranteed to contain at least one element,
+        // because both TargetFps.TARGET_FPS_30 and DepthSensorUsage.DO_NOT_USE
+        // are supported on all ARCore supported devices.
+        List<CameraConfig> cameraConfigList = session.getSupportedCameraConfigs(filter);
+
+        // Use element 0 from the list of returned camera configs. This is because
+        // it contains the camera config that best matches the specified filter
+        // settings.
+        session.setCameraConfig(cameraConfigList.get(0));
     }
 }
